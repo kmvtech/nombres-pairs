cf783abfeebcc799750b0fa0c6451102cefa38b9
KMV Tech <techkmv4@gmail.com>
2025-04-12 08:45:56 +0100

    correction des valeurs negatives

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
+int nombrePositif()
+{   
+    int nombre = 0 ;
+    printf("Entrer un nombre positif :");
+    scanf("%d", &nombre);
+    while (nombre < 0)
+    {
+        printf("vous devez un nombre positif et non negatif :");
+        scanf("%d", &nombre);
+
+    }
+    return nombre ;
+    
+}
 int main()
 {
-    int nombre = 0 ;
-    printf("Emtrez un nombre :");
-    scanf("%d", &nombre);
+    int nombre = 0;
+    nombre = nombrePositif();
+
     printf("Les nombres pairs entre 1 et %d sont : ", nombre);
-    for (int i = 1; i <= nombre; i++)
+    for(int i = 1; i <= nombre; i++)
     {
         /* code */
-        if (i%2 == 0)
+        if(i%2 == 0)
         {
             printf("%d; ", i);
         }
